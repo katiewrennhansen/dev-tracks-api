c4de9aad3b716c89acf42aaae80e9db955d8e1a8
Katie Hansen <khansen0415@gmail.com>
2019-11-25 18:34:52 -0500

    delete slash

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://kawrenn@localhost/dev_tracks',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://kawrenn@localhost/dev_tracks_test',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://dev-track.now.sh/',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://dev-track.now.sh',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api',
     JWT_SECRET: process.env.JWT_SECRET || 'secrets-secrets'
 }
