868172034ade927e27bfb7e2d6d0e62519435c3f
Katie Hansen <khansen0415@gmail.com>
2019-11-25 18:24:13 -0500

    add url

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://kawrenn@localhost/dev_tracks',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://kawrenn@localhost/dev_tracks_test',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://dev-track-nn2ba4gpp.now.sh/',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://dev-track.now.sh/',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://dev-tracks-api.herokuapp.com/',
     JWT_SECRET: process.env.JWT_SECRET || 'secrets-secrets'
 }
